abc7f6aacf9d9eb88aa242eaeb4e1cc2b7cb3afa
sukrity1 <sukrity@rocketmail.com>
2024-01-29 12:17:01 +0530

    Revert "second"
    This reverts commit 3eeb7398fc3ade3684fa1d9b1e06822807725e05.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>checking1</span>
+          Version <span style={{ color: 'red' }}>abcdsinha</span>
         </div>
       </header>
     </div>
