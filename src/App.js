d66f11e83092b5df78d122dd16eadbd38dfac57b
sukrity1 <sukrity@rocketmail.com>
2024-01-29 12:44:32 +0530

    react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>Sukrity</span>
+          Version <span style={{ color: 'red' }}>React</span>
         </div>
       </header>
     </div>
